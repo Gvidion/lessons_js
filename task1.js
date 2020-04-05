3c350c28bdf01be10e913aff60bb4b30a389ae54
Gvidion <Neswion@gmail.com>
2020-04-05 10:17:44 +0300

    did task 1

---
+++ task1.js
@@ -0,0 +1,22 @@
+
+let money = prompt("Ваш бюджет на месяц?", 0);
+let time = prompt("Введите дату в формате YYYY-MM-DD", 0);
+
+let first = prompt("Введите обязательную статью расходов в этом месяце", 0);
+let second = prompt("Во сколько обойдется?", 0);
+
+let expenses = {
+    first: second
+    };
+
+let appData = {
+    budget: money,
+    timeData: time,
+    expense: expenses,
+    optionalExpenses: '',
+    income: '',
+    savings: false
+};
+
+
+

17d0b1b439c63920abab47ea2dc4ce0309f256b2
Gvidion <Neswion@gmail.com>
2020-04-07 17:41:16 +0300

    lesson 2

--- task1.js
+++ task1.js
@@ -1,22 +1,28 @@
 
-let money = prompt("Ваш бюджет на месяц?", 0);
+let money = +prompt("Ваш бюджет на месяц?", 0);
 let time = prompt("Введите дату в формате YYYY-MM-DD", 0);
-
-let first = prompt("Введите обязательную статью расходов в этом месяце", 0);
-let second = prompt("Во сколько обойдется?", 0);
-
-let expenses = {
-    first: second
-    };
 
 let appData = {
     budget: money,
     timeData: time,
-    expense: expenses,
-    optionalExpenses: '',
-    income: '',
+    expense: {},
+    optionalExpenses: {},
+    income: [],
     savings: false
 };
 
+for(let i = 0; i < 2; i++){
+    let a = prompt("Введите обязательную статью расходов в этом месяце", 0),
+        b = prompt("Во сколько обойдется?", 0);
 
+    if ((typeof(a)) === 'string' && (typeof(a) != null) && (typeof(b)) != null
+        && a != '' && b != '' && a.length < 50) {
+        console.log('done');
+        appData.expense[a] = b;
+    } else {
+        i--;
+    }   
+}
 
+appData.moneyPerDay = appData.budget / 30;
+alert("Ежедневный бюджет = " + appData.moneyPerDay);
